8e1edc0180203b2fbe148f51851e657e6ac607d2
viewtifult <tashanduncan1994@gmail.com>
2020-12-05 22:52:54 +0000

    add alert notifications

--- js/app.js
+++ js/app.js
@@ -4,22 +4,37 @@ const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 
 
+
 // create the html for the banner
 alertBanner.innerHTML =`
 <div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+<p><strong>Alert:</strong> You have <strong>3</strong> overdue tasks
 to complete</p>
 <p class="alert-banner-close">x</p>
 </div>
+
 `;
+
+const tasks = document.getElementById("tasks");
+const closeTasks = document.getElementById("close-tasks")
+tasks.style.display = "none"
 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
+    
     if (element.classList.contains("alert-banner-close")) {
     alertBanner.style.display = "none";
+    }  else if (tasks.style.display === "none") {
+        tasks.style.display = ""
     }
 });
 
+closeTasks.addEventListener('click', e => {
+    const element = e.target;
+    const parent = e.target.parentElement;
+
+    parent.style.display = "none";
+})
 
 send.addEventListener('click', () => {
     // ensure user and message fields are filled out
@@ -35,4 +50,4 @@ send.addEventListener('click', () => {
     message.value = '';
     }
 });
-    
+
