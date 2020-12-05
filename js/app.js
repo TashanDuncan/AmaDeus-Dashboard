ee31878d7263119e2187eac8761b296ab6c51e28
viewtifult <tashanduncan1994@gmail.com>
2020-12-05 17:52:46 +0000

    add timezones

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 const alertBanner = document.getElementById("alert");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+
 // create the html for the banner
 alertBanner.innerHTML =`
 <div class="alert-banner">
@@ -14,3 +19,18 @@ alertBanner.addEventListener('click', e => {
     alertBanner.style.display = "none";
     }
 });
+
+
+send.addEventListener('click', () => {
+    // ensure user and message fields are filled out
+    if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+});
+    
